1f1aa59e530ac57d424430d4aa662c6b21022bc9
Anthony Ferritto <aferritto@ibm.com>
2022-03-09 16:04:30 -0500

    mrc trainer test stub

---
+++ tests/oneqa/mrc/trainers/test_default.py
@@ -0,0 +1,9 @@
+import pytest
+
+from tests.oneqa.mrc.common.base import UnitTest
+
+
+@pytest.mark.skip("TODO: implement MRCTrainer tests")
+class TestMRCTrainer(UnitTest):
+    def test_trainer(self):
+        raise NotImplementedError
