26265a0d072ef51202cd01c2d3baf40950683b94
Bhavani Iyer <bsiyer@us.ibm.com>
2022-04-27 13:41:58 -0400

    Add type hints, docstring

--- examples/ir/run_bm25_retrieval.py
+++ examples/ir/run_bm25_retrieval.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import csv
 import json
+from typing import List, Dict
 from oneqa.ir.sparse.retriever import PyseriniRetriever
 import logging
 
@@ -12,16 +13,16 @@ logging.basicConfig(level=logging.INFO)
 def handle_args():
     parser = argparse.ArgumentParser(description='Run BM25 search'
 )
-    parser.add_argument('--index_path', required=True,help="Path to a lucene index")
-    parser.add_argument('--queries_file', required=True, help='Path to queries file in ColBERT tsv [id\tquery] format')
-    parser.add_argument('--top_k', required=False, default=1000, type=int, help='Number of documents/passages to retrieve')
-    parser.add_argument('--xorqa_data_file', required=False, default=None, help="Required to extract language id for XORQA evaluation." )
+    parser.add_argument('--index_path', type=str,required=True,help="Path to a lucene index")
+    parser.add_argument('--queries_file', type=str, required=True, help='Path to queries file in ColBERT tsv [id\tquery] format')
+    parser.add_argument('--top_k', type=int, required=False, default=1000, type=int, help='Number of documents/passages to retrieve')
+    parser.add_argument('--xorqa_data_file', type=str, required=False, default=None, help="Required to extract language id for XORQA evaluation." )
     parser.add_argument('--max_hits_to_output', type=int, required=False, default=100, help='Number of hits to output for XORQA evaluation.')
-    parser.add_argument('--output_dir', required=True, help='Path to the output directory')
+    parser.add_argument('--output_dir', type=str, required=True, help='Path to the output directory')
     args=parser.parse_args()
     return args
  
-def load_queries(filepath):
+def load_queries(filepath: str) -> Dict:
     id_to_question = {}
     with open(filepath,'r') as f:
         for line in f:
@@ -29,9 +30,9 @@ def load_queries(filepath):
             id_to_question[id] = question
     return id_to_question
 
-def run_search(index_path, queries, top_n=1000, k1=0.9, b=0.4):
+def run_search(index_path: str, queries: List, top_n: int = 1000, k1: float = 0.9, b: float = 0.4) -> Dict:
     logger.info(f"Loading index {index_path}")
-    searcher = PyseriniRetriever(index_path, k1=k1, b=b)
+    searcher = PyseriniRetriever(index_path, use_bm25=True, k1=k1, b=b)
     logger.info(f"Running search")
     id_to_hits = {}
     for id in queries:
@@ -41,7 +42,7 @@ def run_search(index_path, queries, top_n=1000, k1=0.9, b=0.4):
         id_to_hits[id] = hits
     return id_to_hits
 
-def write_colbert_ranking_tsv(output_dir, id_to_hits):
+def write_colbert_ranking_tsv(output_dir: str , id_to_hits: Dict):
     output_file = os.path.join(output_dir,'ranking.tsv')
     search_results = []
     for id in id_to_hits:
@@ -60,7 +61,7 @@ def write_colbert_ranking_tsv(output_dir, id_to_hits):
             writer.writerow(r.values())
     logger.info(f"Wrote {output_file}")
         
-def get_language_id(xorqa_data_file):
+def get_language_id(xorqa_data_file: str) -> Dict:
     id_to_lang = {}
     with open(xorqa_data_file,'r') as f:
         for line in f:
@@ -68,7 +69,7 @@ def get_language_id(xorqa_data_file):
             id_to_lang[data['id']] = data['lang']
     return id_to_lang
 
-def write_xorqa_json(output_dir, id_to_hits,top_n=100, xorqa_data_file=None):
+def write_xorqa_json(output_dir: str, id_to_hits: Dict, top_n: int =100, xorqa_data_file: str = None):
     id_to_lang = None
     if xorqa_data_file != None:
         id_to_lang = get_language_id(xorqa_data_file)
@@ -81,14 +82,14 @@ def write_xorqa_json(output_dir, id_to_hits,top_n=100, xorqa_data_file=None):
             "lang" : id_to_lang[id] if id_to_lang != None else "", 
             "ctxs" : []
         }
-        for hit in id_to_hits[id][0:top_n]:
+        for hit in id_to_hits[id][:top_n]:
             json_data["ctxs"].append(hit['text'])
         search_results.append(json_data)
     with open(output_file,'w',encoding='utf-8') as f:
         json.dump(search_results,f, indent=4)
     logger.info(f"Wrote {output_file}")
 
-def write_search_results(id_to_hits, output_dir, max_hits_output, xorqa_data_file=None):
+def write_search_results(id_to_hits: Dict, output_dir: str, max_hits_output: int, xorqa_data_file: str =None):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     write_xorqa_json(output_dir, id_to_hits, top_n=max_hits_output, xorqa_data_file=xorqa_data_file)
--- oneqa/ir/sparse/retriever.py
+++ oneqa/ir/sparse/retriever.py
@@ -13,42 +13,137 @@ class BaseRetriever(metaclass=ABCMeta):
 
     @abstractmethod
     def retrieve(self, query: str, top_k: Optional[int] = 10):
+        """
+
+        Run queries against the index to retrieve ranked list of documents
+        Return documents that are most relevant to the query.
+
+        Args:
+             query: search
+             top_k: number of hits to return, defaults to 10
+
+
+        Returns:
+             List of hits, each hit is a dict containing :
+             {
+                "rank": i,
+                "score": hit.score,
+                "passage_id": docid,
+                "doc_id": docid,
+                "title": title,
+                "text": text 
+            }
+                
+
+        """
         pass
 
-    def batch_retrieve(self,  queries: List[str], qids: List[str], k: int = 10, threads: int = 1):
+    @abstractmethod
+    def batch_retrieve(self,  queries: List[str], qids: List[str], top_k: int = 10, threads: int = 1):
+        """
+           Run a batch of queries 
+
+           Args:
+                queries:  list of query strings
+                qids:     list of corresponding query ids
+                top_k:    number of hits to return, defaults to 10
+                threads:  maximum number of threads to use
+                
+            Returns;
+                Dict of query to hits where a hit is a dict of key-values:
+                {
+                    "rank": i,
+                    "score": hit.score,
+                    "passage_id": docid,
+                    "doc_id": docid,
+                    "title": title,
+                    "text": text 
+               }
+                
+        """
         pass
 
 class PyseriniRetriever(BaseRetriever):
-    def __init__(self, index_path: str, use_bm25=True, k1=float(0.9), b=float(0.4)):
+    def __init__(self, index_path: str, use_bm25: bool = True, k1: float = float(0.9), b: float = float(0.4)):
+        """
+        Initialize Pyserini retriever
+
+        Args:
+            index_path (str): Path to a Pyserini index
+            use_bm25 (bool, optional): set BM25 as the scoring function. Defaults to True.
+            k1 (float, optional): bm25 parameter to tune impact of term frequency Defaults to float(0.9).
+            b (float, optional): bm25 constant to fine tune the effect of document length   Defaults to float(0.4).
+        """
         self.index_path = index_path
         self.searcher = LuceneSearcher(index_path)
         self.searcher.set_bm25()
         if use_bm25:
             self.searcher.set_bm25(k1=k1,b=b)
         self.top_k = 10
-        logger.info(f'Initialized LuceneSearcher index_dir: {self.searcher.index_dir}  num_docs: {self.searcher.num_docs} k1: {k1} b: {b}')
+        logger.info(f'Initialized LuceneSearcher index_dir: {self.searcher.index_dir}  num_docs: {self.searcher.num_docs} use_bm25: {use_bm25} k1: {k1} b: {b}')
 
     def retrieve(self, query: str, top_k: Optional[int] = 10):
         """
+
+        Run queries against the index to retrieve ranked list of documents
         Return documents that are most relevant to the query.
 
-        :param query: The query
-        :param top_k: How many documents to return per query.
+        Args:
+             query: search
+             top_k: number of hits to return, defaults to 10
+
+
+        Returns:
+             List of hits, each hit is a dict containing :
+             {
+                "rank": i,
+                "score": hit.score,
+                "passage_id": docid,
+                "doc_id": docid,
+                "title": title,
+                "text": text 
+            }
+                
+
         """
 
         hits = self.searcher.search(query, top_k)
         passage_hits = self._collect_hits(hits)
         return passage_hits
 
-    def batch_retrieve(self,  queries: List[str], qids: List[str], k: int = 10, threads: int = 1):
-        query_hits = self.searcher.batch_retrieve(queries, qids, k=k, threads=threads)
+
+    def batch_retrieve(self,  queries: List[str], qids: List[str], top_k: int = 10, threads: int = 1):
+
+        """
+           Run a batch of queries 
+
+           Args:
+                queries:  list of query strings
+                qids:     list of corresponding query ids
+                top_k:    number of hits to return, defaults to 10
+                threads:  maximum number of threads to use
+                
+            Returns;
+                Dict of query to hits where a hit is a dict of key-values:
+                {
+                    "rank": i,
+                    "score": hit.score,
+                    "passage_id": docid,
+                    "doc_id": docid,
+                    "title": title,
+                    "text": text 
+               }
+                
+        """
+
+        query_hits = self.searcher.batch_retrieve(queries, qids, k=top_k, threads=threads)
         query_to_passage_hits = {}
         for q, hits in query_hits.item():
             query_to_passage_hits[q] = self._collect_hits(hits)
         return query_to_passage_hits
 
 
-    def _collect_hits(self, hits):
+    def _collect_hits(self, hits: List):
         passage_hits = []
         for i, hit in enumerate(hits):
             title, text = json.loads(hit.raw)['contents'].split("\t")
