ed17c6fbde5d754aa08144bc430e82c9f12f7db5
Vishwajeet Kumar <vishk024@in.ibm.com>
2022-05-05 14:36:52 +0530

    Added qg trainer, qg model and qg module in examples folder

---
+++ examples/tableqg/run_tableqg.py
@@ -0,0 +1,114 @@
+from transformers import (
+    HfArgumentParser,
+    DataCollator,
+    TrainingArguments,
+    set_seed,
+)
+
+from dataclasses import dataclass,field
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+@dataclass
+class T2TDataCollator(DataCollator):
+    def collate_batch(self, batch: List) -> Dict[str, torch.Tensor]:
+        """
+        Take a list of samples from a Dataset and collate them into a batch.
+        Returns:
+            A dictionary of tensors
+        """
+        input_ids = torch.stack([example['input_ids'] for example in batch])
+        lm_labels = torch.stack([example['target_ids'] for example in batch])
+        lm_labels[lm_labels[:, :] == 0] = -100
+        attention_mask = torch.stack([example['attention_mask'] for example in batch])
+        decoder_attention_mask = torch.stack([example['target_attention_mask'] for example in batch])
+        
+
+        return {
+            'input_ids': input_ids, 
+            'attention_mask': attention_mask,
+            'lm_labels': lm_labels, 
+            'decoder_attention_mask': decoder_attention_mask
+        }
+
+
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
+    """
+
+    model_name_or_path: str = field(
+        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
+    )
+    tokenizer_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+    )
+    cache_dir: Optional[str] = field(
+        default=None, metadata={"help": "Where do you want to store the pretrained models downloaded from s3"}
+    )
+
+@dataclass
+class DataTrainingArguments:
+    """
+    Arguments pertaining to what data we are going to input our model for training and eval.
+    """
+    train_file_path: Optional[str] = field(
+        default='train_data.pt',
+        metadata={"help": "Path for cached train dataset"},
+    )
+    valid_file_path: Optional[str] = field(
+        default='valid_data.pt',
+        metadata={"help": "Path for cached valid dataset"},
+    )
+    max_len: Optional[int] = field(
+        default=512,
+        metadata={"help": "Max input length for the source text"},
+    )
+    target_max_len: Optional[int] = field(
+        default=32,
+        metadata={"help": "Max input length for the target text"},
+    )
+
+
+def main():
+    # See all possible arguments in src/transformers/training_args.py
+    # or by passing the --help flag to this script.
+    # We now keep distinct sets of args, for a cleaner separation of concerns.
+
+    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
+
+    # we will load the arguments from a json file, 
+    #make sure you save the arguments in at ./args.json
+    model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath('args.json'))
+    if (
+        os.path.exists(training_args.output_dir)
+        and os.listdir(training_args.output_dir)
+        and training_args.do_train
+        and not training_args.overwrite_output_dir
+    ):
+        raise ValueError(
+            f"Output directory ({training_args.output_dir}) already exists and is not empty. Use --overwrite_output_dir to overcome."
+        )
+
+    # Setup logging
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO if training_args.local_rank in [-1, 0] else logging.WARN,
+    )
+    logger.warning(
+        "Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s",
+        training_args.local_rank,
+        training_args.device,
+        training_args.n_gpu,
+        bool(training_args.local_rank != -1),
+        training_args.fp16,
+    )
+    logger.info("Training/evaluation parameters %s", training_args)
+
+    # Set seed
+    set_seed(training_args.seed)
+
