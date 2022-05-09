78349576d04f6d2a5eefe20e3ce5cd85ae1b9f95
Vishwajeet Kumar <vishk024@in.ibm.com>
2022-05-09 15:08:30 +0530

    Used automodel and autotokenizer so that we can support any model like bert, bart, t5 and mt5

--- oneqa/tableqg/models/tableqg_model.py
+++ oneqa/tableqg/models/tableqg_model.py
@@ -1,11 +1,10 @@
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import AutoTokenizer,AutoConfig, AutoModel
 
-
-class TableQG(T5ForConditionalGeneration):
+class TableQG():
     def __init__(self,model_path):
         super.__init__()
-        self.model = T5ForConditionalGeneration.from_pretrained(model_path)
-        self.tokenizer = T5Tokenizer.from_pretrained(model_path)
+        self.model = AutoModel.from_pretrained(model_path)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
     
     def get_model(self):
         return self.model
