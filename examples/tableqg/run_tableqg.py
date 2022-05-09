9f77796c2b50b268516e34a3b3b006cb78e5bbd2
Vishwajeet Kumar <vishk024@in.ibm.com>
2022-05-09 23:01:10 +0530

    added args.json and completed run_tableqg.py code with trainer and models from oneqa directory

--- examples/tableqg/run_tableqg.py
+++ examples/tableqg/run_tableqg.py
@@ -6,8 +6,12 @@ from transformers import (
 )
 
 from dataclasses import dataclass,field
+from oneqa.tableqg.models.tableqg_model import TableQG
+from oneqa.tableqg.trainers.qg_trainer import QGTrainer
 
 import logging
+
+import os
 
 logger = logging.getLogger(__name__)
 
@@ -47,7 +51,7 @@ class ModelArguments:
         default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
     )
     cache_dir: Optional[str] = field(
-        default=None, metadata={"help": "Where do you want to store the pretrained models downloaded from s3"}
+        default=None, metadata={"help": "Where do we want to store the pretrained models downloaded from s3"}
     )
 
 @dataclass
@@ -74,14 +78,10 @@ class DataTrainingArguments:
 
 
 def main():
-    # See all possible arguments in src/transformers/training_args.py
-    # or by passing the --help flag to this script.
-    # We now keep distinct sets of args, for a cleaner separation of concerns.
 
     parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
 
-    # we will load the arguments from a json file, 
-    #make sure you save the arguments in at ./args.json
+    # We need to load the config hyperparameter from args file path
     model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath('args.json'))
     if (
         os.path.exists(training_args.output_dir)
@@ -111,4 +111,50 @@ def main():
 
     # Set seed
     set_seed(training_args.seed)
+
+    tqg = TableQG(model_args.model_name_or_path)
+    model = tqg.model
+    tokenizer = tqg.tokenizer
+
+    #Load the dataset
+    train_dataset  = torch.load(data_args.train_file_path)
+    valid_dataset = torch.load(data_args.valid_file_path)
+
+    trainer = QGTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=valid_dataset,
+        data_collator=T2TDataCollator(),
+        prediction_loss_only=True
+    )
+    if training_args.do_train:
+        trainer.train(
+        model_path=model_args.model_name_or_path if os.path.isdir(model_args.model_name_or_path) else None
+    )
+    trainer.save_model()
+    if trainer.is_world_master():
+        tokenizer.save_pretrained(training_args.output_dir)
+
+    # Evaluation
+    results = {}
+    if training_args.do_eval and training_args.local_rank in [-1, 0]:
+        logger.info("*** Evaluate ***")
+
+        eval_output = trainer.evaluate()
+
+        output_eval_file = os.path.join(training_args.output_dir, "eval_results.txt")
+        with open(output_eval_file, "w") as writer:
+            logger.info("***** Eval results *****")
+            for key in sorted(eval_output.keys()):
+                logger.info("  %s = %s", key, str(eval_output[key]))
+                writer.write("%s = %s\n" % (key, str(eval_output[key])))
+    
+        results.update(eval_output)
+    
+    return results
+
+
+if __name__ == "__main__":
+    main()
 
