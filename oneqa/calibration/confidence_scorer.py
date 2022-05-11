149b0ca330537385e158c1e2dd7f9780444aa076
zhrong <zhangr@us.ibm.com>
2022-05-11 12:01:22 -0400

    call make_features() from make_training_data() to remove duplicated code

--- oneqa/calibration/confidence_scorer.py
+++ oneqa/calibration/confidence_scorer.py
@@ -9,7 +9,7 @@ from oneqa.mrc.data_models.target_type import TargetType
 
 
 class ConfidenceScorer(object):
-    def __init__(self, confidence_model_path):
+    def __init__(self, confidence_model_path=None):
         if confidence_model_path:
             try:
                 if os.path.isdir(confidence_model_path):
@@ -22,12 +22,10 @@ class ConfidenceScorer(object):
             self._confidence_model = None
 
     def model_exists(self):
-        if self._confidence_model is not None:
-            return True
-        else:
-            return False
+        return self._confidence_model is not None
 
-    def make_features(self, example_predictions):
+    @classmethod
+    def make_features(cls, example_predictions):
         # minimum risk f1
         minimum_risk_f1 = []
         for i in range(len(example_predictions)):
@@ -71,7 +69,6 @@ class ConfidenceScorer(object):
         for pred in example_predictions:
             average_norm_span_answer_score += pred["normalized_span_answer_score"]
         average_norm_span_answer_score /= len(example_predictions)
-
         features = []
         for i, pred in enumerate(example_predictions):
             feat = []
@@ -93,8 +90,8 @@ class ConfidenceScorer(object):
         return features
 
     def predict_scores(self, example_predictions):
-        if not self.model_exists():
-            return [0.0] * len(example_predictions)
+        if example_predictions is None:
+            return None
         features = self.make_features(example_predictions)
         if len(features) == 0:
             return [0.0] * len(example_predictions)
@@ -102,6 +99,8 @@ class ConfidenceScorer(object):
         X = np.zeros((len(features), feature_dimension), dtype=np.double)
         for i, feat in enumerate(features):
             X[i, :] = feat
+        if not self.model_exists():
+            return [0.0] * len(example_predictions)
         scores = self._confidence_model.predict_proba(X)
         # scores[:,0] : scores for incorrect, scores[:, 1]: score for correct
         return scores[:, 1]
@@ -140,7 +139,6 @@ class ConfidenceScorer(object):
                 raw_references = json.load(f)
         except:
             raise ValueError("Unable to load reference file to create training data for confidence model")
-
         references = dict()
         for raw_ref in raw_references:
             example_id = raw_ref["example_id"][0]
@@ -154,93 +152,30 @@ class ConfidenceScorer(object):
                 span["end_position"] = raw_ref["end_position"][i]
                 ref["span_answer"].append(span)
                 references[example_id] = ref
-
         try:
             with open(prediction_file, 'r') as f:
                 raw_predictions = json.load(f)
         except:
             raise ValueError("Unable to load prediction file to create training data for confidence model")
-
         feature_set = dict()
         label_set = dict()
         for example_id in raw_predictions:
             top_k_predictions = raw_predictions[example_id]
-
-            # minimum risk f1
-            minimum_risk_f1 = []
-            for i in range(len(top_k_predictions)):
-                sum = 0.0
-                for j in range(len(top_k_predictions)):
-                    if j == i:
-                        continue
-                    s1 = top_k_predictions[i]["span_answer"]["start_position"]
-                    e1 = top_k_predictions[i]["span_answer"]["end_position"]
-                    s2 = top_k_predictions[j]["span_answer"]["start_position"]
-                    e2 = top_k_predictions[j]["span_answer"]["end_position"]
-                    if s1 == s2 and e1 == e2:
-                        sum += 1.0
-                    elif s1 > e2 or e1 < s2:
-                        continue
-                    else:
-                        overlap_start_position = max(s1, s2)
-                        overlap_end_position = min(e1, e2)
-                        precision = float(overlap_end_position - overlap_start_position + 1) / float(e1 - s1 + 1)
-                        recall = float(overlap_end_position - overlap_start_position + 1) / float(e2 - s2 + 1)
-                        f1 = (2 * precision * recall) / (precision + recall)
-                        sum += f1
-                sum /= len(top_k_predictions)
-                minimum_risk_f1.append(sum)
-
-            # if have span answer
-            have_span_answer = []
-            for pred in top_k_predictions:
-                if "target_type_logits" not in pred:
-                    pred["target_type_logits"] = [0, 0, 0, 0, 0]
-                if pred["target_type_logits"][TargetType.SPAN_ANSWER] == max(pred["target_type_logits"]):
-                    have_span_answer.append(1.0)
-                else:
-                    have_span_answer.append(0.0)
-            if max(have_span_answer) == 1.0:
-                example_have_span_answer = 1.0
-            else:
-                example_have_span_answer = 0.0
-
-            average_norm_span_answer_score = 0.0
-            for pred in top_k_predictions:
-                average_norm_span_answer_score += pred["normalized_span_answer_score"]
-            average_norm_span_answer_score /= len(top_k_predictions)
-
-            feat = []
-            pred = top_k_predictions[0]
-            feat.append(pred["span_answer_score"])
-            feat.append(pred["cls_score"])
-            feat.append(pred["start_logit"])
-            feat.append(pred["end_logit"])
-            feat.append(pred["target_type_logits"][TargetType.NO_ANSWER])   # no answer
-            feat.append(pred["target_type_logits"][TargetType.SPAN_ANSWER])   # span answer
-            feat.append(have_span_answer[0])
-            feat.append(example_have_span_answer)
-            feat.append(minimum_risk_f1[0])
-            feat.append(pred["normalized_span_answer_score"])
-            feat.append(pred["normalized_span_answer_score"] - average_norm_span_answer_score)
-            feat.append(pred["start_stdev"])
-            feat.append(pred["end_stdev"])
-            feat.append(pred["query_passage_similarity"])
-            feature_set[example_id] = feat
+            features_of_top_k_predictions = cls.make_features(top_k_predictions)
+            # only use top-1 features for training
+            feature_set[example_id] = features_of_top_k_predictions[0]
 
             overlap_score = cls.reference_prediction_overlap(references[example_id]["span_answer"],
-                                                              pred["span_answer"])
+                                                             top_k_predictions[0]["span_answer"])
             if overlap_score >= overlap_threshold:
                 label_set[example_id] = 1
             else:
                 label_set[example_id] = 0
-
         for example_id in feature_set:
             number_features_per_example = len(feature_set[example_id])
             break
         X = np.zeros((len(feature_set), number_features_per_example), dtype=np.double)
         Y = np.zeros((len(feature_set)), dtype=np.int)
-
         for i, example_id in enumerate(sorted(feature_set.keys())):
             X[i, :] = feature_set[example_id]
             Y[i] = label_set[example_id]
