14f5ede7739f6d44c6db132a52bfcf3f81b1acf8
Vishwajeet Kumar <vishk024@in.ibm.com>
2022-05-11 15:49:40 +0530

    corrected spelling on bleuscore

--- oneqa/tableqg/metrics/generation_metrics.py
+++ oneqa/tableqg/metrics/generation_metrics.py
@@ -1,18 +1,18 @@
 import nltk
 import numpy as np
 
-def bluescore(pred_list):
+def bleuscore(pred_list):
 	hyp = [p['predictions'][0] for p in pred_list]
 	ref = [p['quesion'] for p in pred_list]
 
 	hyp = [h.lower().split(' ') for h in hyp]
 	ref = [r.lower().split(' ') for r in ref]
 
-	bluescore = []
+	bleuscore = []
 	for weights in [(1,0,0,0),(0,1,0,0),(0,0,1,0),(0,0,0,1)]:
 		score_list = []
 		for i in range(len(hyp)):
 			score = nltk.translate.bleu_score.sentence_bleu([ref[i]], hyp[i], weights)
 			score_list.append(score)
-		bluescore.append(np.mean(score_list))
-	return bluescore
+		bleuscore.append(np.mean(score_list))
+	return bleuscore
