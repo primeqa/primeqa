a813a7ee6ed79416e1070c53545fc6fe04adb29c
Vishwajeet Kumar <vishk024@in.ibm.com>
2022-05-16 11:02:40 +0530

    created similar folder structure for the tests as well and added script to test sql_sampler

---
+++ tests/oneqa/tableqg/models/test_sql_sampler.py
@@ -0,0 +1,33 @@
+import pytest
+from oneqa.tableqg.models.sql_sampler import SimpleSqlSampler
+
+class TestSQLSampler():
+
+    sql_sampler = SimpleSqlSampler()
+    table = {"header": ["Player", "No.", "Nationality", "Position", "Years in Toronto", "School Team"],
+            "rows": [
+        ["Antonio Lang", 21, "United States", "Guard-Forward", "1999-2000", "Duke"],
+        ["Voshon Lenard", 2, "United States", "Guard", "2002-03", "Minnesota"],
+        ["Martin Lewis", 32, "United States", "Guard-Forward", "1996-97", "Butler CC (KS)"],
+        ["Brad Lohaus", 33, "United States", "Forward-Center", "1996", "Iowa"],
+        ["Art Long", 42, "United States", "Forward-Center", "2002-03", "Cincinnati"]
+        ],
+        "types":["text","real","text","text","text","text"]
+        }
+
+
+    def test_add_column_types(self):
+        assert self.sql_sampler.add_column_types(self.table)['types'] == ['text', 'real', 'text', 'text', 'text', 'text']
+
+
+    def test_sample_sql(self):
+        sampled_sql_str,sampled_sql = self.sql_sampler.sample_sql(self.table,2,2,0)
+        assert len(sampled_sql_str)==2
+        assert len(sampled_sql[0]['conds'])==2
+
+
+        
+
+
+
+    
