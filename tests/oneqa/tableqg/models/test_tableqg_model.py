0c3c5897a93f1e52004410c6bce9839f74384289
Vishwajeet Kumar <vishk024@in.ibm.com>
2022-05-18 14:55:35 +0530

    Added test cases for tableqg coverage is 68%

--- tests/oneqa/tableqg/models/test_tableqg_model.py
+++ tests/oneqa/tableqg/models/test_tableqg_model.py
@@ -1,2 +1,10 @@
 import pytest
+from oneqa.tableqg.models.tableqg_model import TableQG
+from transformers import T5Tokenizer, T5ForConditionalGeneration
 
+
+@pytest.mark.parametrize("model_name",["t5-base"])
+def test_table_qg_model(model_name):
+    tqm = TableQG(model_name)
+    assert type(tqm.model)==T5ForConditionalGeneration
+    assert type((tqm.tokenizer)==T5Tokenizer)
