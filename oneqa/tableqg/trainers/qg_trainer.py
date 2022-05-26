b411e7d15d578302a6375241d07464440005bacf
Vishwajeet Kumar <vishk024@in.ibm.com>
2022-05-26 14:54:43 +0530

    Added and corrected documentation for trainer

--- oneqa/tableqg/trainers/qg_trainer.py
+++ oneqa/tableqg/trainers/qg_trainer.py
@@ -4,11 +4,13 @@ from transformers import Trainer
 class QGTrainer(Trainer):
 
     def __init__(self,*args,train_dataset,valid_dataset,data_collator,**kwargs):
-        """ QG training and Evaluation
+        """Question Generation Trainer for training and Evaluation. This class extends the Trainer class from huggingface.
 
         Args:
-            Trainer (_type_)): _description_
-        """
+            train_dataset (Dataset): Train Dataset Generator
+            valid_dataset (Dataset): Validation Dataset Generator
+            data_collator (Dataset): Test Dataset Generator
+        """  
         super().__init__(*args,train_dataset=train_dataset,eval_dataset=valid_dataset,data_collator=data_collator,**kwargs)
 
 
