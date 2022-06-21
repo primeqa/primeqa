a0ce7b021b0f91372b80ece27c1d0acde93aef65
bhavani105 <69366014+bhavani105@users.noreply.github.com>
2022-06-21 14:59:38 -0400

    Update mlqa.py
    Signed-off-by: bhavani105 <69366014+bhavani105@users.noreply.github.com>

--- primeqa/mrc/processors/preprocessors/mlqa.py
+++ primeqa/mrc/processors/preprocessors/mlqa.py
@@ -14,7 +14,9 @@ class MLQAPreprocessor(SQUADPreprocessor):
         # zh: context language; en: question language
         # the answer language is the context language
         answer_language = dataset.config_name[5:7]
-        dataset = dataset.map(lambda example: {'answer_language': answer_language})
+        dataset = dataset.map(lambda example: {'answer_language': answer_language},
+                              load_from_cache_file=self._load_from_cache_file,
+                              num_proc=self._num_workers)
         return dataset
     
 
