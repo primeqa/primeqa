1d752b9ad3cbe73047b83d921f2b3c124c4e5419
mabornea <mabornea@us.ibm.com>
2022-06-21 13:50:06 -0400

    Change in the MLQAPreprocessor to read from cache

--- primeqa/mrc/processors/preprocessors/mlqa.py
+++ primeqa/mrc/processors/preprocessors/mlqa.py
@@ -9,12 +9,14 @@ class MLQAPreprocessor(SQUADPreprocessor):
         super().__init__(*args, **kwargs)
 
     def adapt_dataset(self, dataset: Dataset, is_train: bool) -> Dataset:
-        dataset = super().adapt_dataset(dataset, is_train)
         # Example of the dataset config name: mlqa.zh.en
         # zh: context language; en: question language
         # the answer language is the context language
         answer_language = dataset.config_name[5:7]
-        dataset = dataset.map(lambda example: {'answer_language': answer_language})
+        dataset = dataset.map(lambda example: {'answer_language': answer_language},
+                              load_from_cache_file=self._load_from_cache_file,
+                              num_proc=self._num_workers)
+        dataset = super().adapt_dataset(dataset, is_train)
         return dataset
     
 
