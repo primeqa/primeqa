36258789b8e327ca8a8b95559341ab2781200725
Jaydeep Sen <jaydesen@in.ibm.com>
2022-06-29 00:14:59 +0530

    PrimeQA replacement fix
    Signed-off-by: Jaydeep Sen <jaydesen@in.ibm.com>

--- primeqa/ir/dense/colbert_top/colbert/modeling/tokenization/__init__.py
+++ primeqa/ir/dense/colbert_top/colbert/modeling/tokenization/__init__.py
@@ -1,3 +1,3 @@
-from oneqa.ir.dense.colbert_top.colbert.modeling.tokenization.query_tokenization import *
-from oneqa.ir.dense.colbert_top.colbert.modeling.tokenization.doc_tokenization import *
-from oneqa.ir.dense.colbert_top.colbert.modeling.tokenization.utils import tensorize_triples
+from primeqa.ir.dense.colbert_top.colbert.modeling.tokenization.query_tokenization import *
+from primeqa.ir.dense.colbert_top.colbert.modeling.tokenization.doc_tokenization import *
+from primeqa.ir.dense.colbert_top.colbert.modeling.tokenization.utils import tensorize_triples
