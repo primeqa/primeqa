ff64cbd0da0ff110c6fd35b41e1c4c0fc05d6a31
zhrong <zhangr@us.ibm.com>
2022-07-05 10:06:11 -0400

    added supports for NQ processing

--- examples/mrc/run_mrc.py
+++ examples/mrc/run_mrc.py
@@ -12,6 +12,7 @@ from operator import attrgetter
 from typing import Optional, Type
 
 import datasets
+import apache_beam as beam
 from transformers import HfArgumentParser, TrainingArguments, DataCollatorWithPadding, AutoConfig, AutoTokenizer
 from transformers.trainer_utils import get_last_checkpoint, set_seed
 
@@ -19,6 +20,7 @@ from primeqa.mrc.data_models.eval_prediction_with_processing import EvalPredicti
 from primeqa.mrc.metrics.tydi_f1.tydi_f1 import TyDiF1
 from primeqa.mrc.metrics.mlqa.mlqa import MLQA
 from primeqa.mrc.metrics.squad.squad import SQUAD
+from primeqa.mrc.metrics.nq_f1.nq_f1 import NQF1
 from primeqa.mrc.models.heads.extractive import EXTRACTIVE_HEAD, EXTRACTIVE_WITH_CONFIDENCE_HEAD
 from primeqa.mrc.models.task_model import ModelForDownstreamTasks
 from primeqa.mrc.processors.postprocessors.extractive import ExtractivePostProcessor
@@ -27,6 +29,8 @@ from primeqa.mrc.processors.postprocessors.scorers import SupportedSpanScorers
 from primeqa.mrc.processors.preprocessors.tydiqa import TyDiQAPreprocessor
 from primeqa.mrc.processors.preprocessors.squad import SQUADPreprocessor
 from primeqa.mrc.processors.postprocessors.squad import SQUADPostProcessor
+from primeqa.mrc.processors.preprocessors.natural_questions import NaturalQuestionsPreProcessor
+from primeqa.mrc.processors.postprocessors.natural_questions import NaturalQuestionsPostProcessor
 from primeqa.mrc.trainers.mrc import MRCTrainer
 from examples.boolqa.run_boolqa_classifier import main as cls_main
 from examples.boolqa.run_score_normalizer import main as sn_main
@@ -197,6 +201,12 @@ class DataTrainingArguments:
                     "Otherwise it will be discarded."
         },
     )
+    beam_runner: str = field(
+        default=None,
+        metadata={"help": "The beam runner for loading large dataset.",
+                  "choices": 'DirectRunner',
+                  }
+    )
 
 
 @dataclass
@@ -219,19 +229,19 @@ class TaskArguments:
     preprocessor: object_reference = field(
         default=TyDiQAPreprocessor,
         metadata={"help": "The name of the preprocessor to use.",
-                  "choices": [TyDiQAPreprocessor,SQUADPreprocessor]
+                  "choices": [TyDiQAPreprocessor,SQUADPreprocessor, NaturalQuestionsPreProcessor]
                   }
     )
     postprocessor: object_reference = field(
         default=ExtractivePostProcessor,
         metadata={"help": "The name of the postprocessor to use.",
-                  "choices": [ExtractivePostProcessor,ExtractivePipelinePostProcessor,SQUADPostProcessor]
+                  "choices": [ExtractivePostProcessor,ExtractivePipelinePostProcessor,SQUADPostProcessor, NaturalQuestionsPostProcessor]
                   }
     )
     eval_metrics: str = field(
         default="TyDiF1",
         metadata={"help": "The name of the evaluation metric function implemented in primeqa (e.g. TyDiF1).",
-                  "choices": ["TyDiF1","SQUAD","MLQA"]
+                  "choices": ["TyDiF1","SQUAD","MLQA","NQF1"]
                  }
     )
     do_boolean: bool = field(
@@ -352,11 +362,20 @@ def main():
             data_files=data_files,
             cache_dir=model_args.cache_dir)
     else:
-        raw_datasets = datasets.load_dataset(
-            data_args.dataset_name,
-            data_args.dataset_config_name,
-            cache_dir=model_args.cache_dir,
-        )
+        if data_args.beam_runner:
+            raw_datasets = datasets.load_dataset(
+                data_args.dataset_name,
+                data_args.dataset_config_name,
+                cache_dir=model_args.cache_dir,
+                beam_runner=data_args.beam_runner
+            )
+        else:
+            raw_datasets = datasets.load_dataset(
+                data_args.dataset_name,
+                data_args.dataset_config_name,
+                cache_dir=model_args.cache_dir
+            )
+
 
     # load preprocessor
     preprocessor_class = task_args.preprocessor
