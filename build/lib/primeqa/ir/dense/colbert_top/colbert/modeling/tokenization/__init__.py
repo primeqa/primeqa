8c3290f9f2b5418d889e62e2902941873d489281
Juan B <juanbrusco_23@hotmail.com>
2022-08-16 15:15:36 -0300

    remove files

--- build/lib/primeqa/ir/dense/colbert_top/colbert/modeling/tokenization/__init__.py
+++
@@ -1,3 +0,0 @@
-from primeqa.ir.dense.colbert_top.colbert.modeling.tokenization.query_tokenization import *
-from primeqa.ir.dense.colbert_top.colbert.modeling.tokenization.doc_tokenization import *
-from primeqa.ir.dense.colbert_top.colbert.modeling.tokenization.utils import tensorize_triples
