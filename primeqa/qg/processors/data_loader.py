760b48215859f1fa3416bf6bd111212555876b98
Maximilian Schmidt <maximilian.schmidt@ims.uni-stuttgart.de>
2022-08-17 18:09:48 +0200

    Remove todo, add note and run black

--- primeqa/qg/processors/data_loader.py
+++ primeqa/qg/processors/data_loader.py
@@ -1,27 +1,30 @@
 from datasets import load_dataset
-
-from .passage_qg.qg_processor import QGProcessor
 from primeqa.qg.processors.table_qg.sql_processor import SqlProcessor
 
+from .passage_qg.qg_processor import QGProcessor
 
-class QGDataLoader():
-	def __init__(self, 
-				tokenizer,
-				modality,
-				input_max_len,
-				target_max_len,
-				dataset_name=None,
-				):
 
-		self.dataset_name = dataset_name
-		# TODO might need to convert data to common format first
-		if modality == 'table':
-			self.processor = SqlProcessor(tokenizer, input_max_len, target_max_len)
-		elif modality == 'passage':
-			self.processor = QGProcessor(tokenizer, input_max_len, target_max_len)
+class QGDataLoader:
+    def __init__(
+        self,
+        tokenizer,
+        modality,
+        input_max_len,
+        target_max_len,
+        dataset_name=None,
+    ):
 
-	def create(self, dataset=None, data_split='train', data_config=None):
-		if dataset is None:
-			assert self.dataset_name is not None
-			dataset = load_dataset(self.dataset_name, name=data_config, split=data_split)
-		return self.processor(dataset)
+        self.dataset_name = dataset_name
+        if modality == "table":
+            self.processor = SqlProcessor(tokenizer, input_max_len, target_max_len)
+        elif modality == "passage":
+            self.processor = QGProcessor(tokenizer, input_max_len, target_max_len)
+
+    def create(self, dataset=None, data_split="train", data_config=None):
+        if dataset is None:
+            assert self.dataset_name is not None
+            dataset = load_dataset(
+                self.dataset_name, name=data_config, split=data_split
+            )
+        # NOTE works only if data has correct format
+        return self.processor(dataset)
--- primeqa/qg/run_qg.py
+++ primeqa/qg/run_qg.py
@@ -11,10 +11,15 @@ from primeqa.qg.metrics.generation_metrics import rouge_metrics
 from primeqa.qg.models.qg_model import QGModel
 from primeqa.qg.processors.data_loader import QGDataLoader
 from primeqa.qg.utils.data_collator import T2TDataCollator
-from transformers import (HfArgumentParser, Seq2SeqTrainer,
-                          Seq2SeqTrainingArguments, set_seed)
+from transformers import (
+    HfArgumentParser,
+    Seq2SeqTrainer,
+    Seq2SeqTrainingArguments,
+    set_seed,
+)
 
 logger = logging.getLogger(__name__)
+
 
 @dataclass
 class ModelArguments:
@@ -23,19 +28,32 @@ class ModelArguments:
     """
 
     model_name_or_path: str = field(
-       default='t5-base', metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models" ,
-                                    "choices":["t5-base", "t5-small", "google/mt5-small","google/mt5-base"]}
+        default="t5-base",
+        metadata={
+            "help": "Path to pretrained model or model identifier from huggingface.co/models",
+            "choices": ["t5-base", "t5-small", "google/mt5-small", "google/mt5-base"],
+        },
     )
     modality: str = field(
-       default='table', metadata={"help": "Whether to generate questions from tables or passages",
-                                  "choices":["table", "passage"]}
+        default="table",
+        metadata={
+            "help": "Whether to generate questions from tables or passages",
+            "choices": ["table", "passage"],
+        },
     )
     tokenizer_name: Optional[str] = field(
-        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+        default=None,
+        metadata={
+            "help": "Pretrained tokenizer name or path if not the same as model_name"
+        },
     )
     cache_dir: Optional[str] = field(
-        default=None, metadata={"help": "Where do we want to store the pretrained models downloaded from s3"}
+        default=None,
+        metadata={
+            "help": "Where do we want to store the pretrained models downloaded from s3"
+        },
     )
+
 
 @dataclass
 class DataTrainingArguments:
@@ -45,18 +63,23 @@ class DataTrainingArguments:
 
     dataset_name: Optional[str] = field(
         default=None,
-        metadata={"help": "Name of the dataset to train the qg model", "choices": list_datasets()}
+        metadata={
+            "help": "Name of the dataset to train the qg model",
+            "choices": list_datasets(),
+        },
     )
     dataset_config: Optional[str] = field(
         default=None,
-        metadata={"help": "Config of the dataset loaded, e.g. 'secondary_task' for TyDiQA"}
+        metadata={
+            "help": "Config of the dataset loaded, e.g. 'secondary_task' for TyDiQA"
+        },
     )
     train_file_path: Optional[str] = field(
-        default='train_data.pt',
+        default="train_data.pt",
         metadata={"help": "Path for cached train dataset"},
     )
     valid_file_path: Optional[str] = field(
-        default='valid_data.pt',
+        default="valid_data.pt",
         metadata={"help": "Path for cached valid dataset"},
     )
     max_len: Optional[int] = field(
@@ -68,44 +91,68 @@ class DataTrainingArguments:
         metadata={"help": "Max input length for the target text"},
     )
 
+
 @dataclass
 class InferenceArguments:
     do_generate: Optional[bool] = field(
         default=False, metadata={"help": "Whether to generate questions"}
     )
     num_questions_per_instance: Optional[int] = field(
-        default=5, metadata={"help": "Number of questions to generate per table/passage"}
+        default=5,
+        metadata={"help": "Number of questions to generate per table/passage"},
     )
     max_where_clauses: Optional[int] = field(
         default=1, metadata={"help": "Max number of filters in generated question"}
     )
-    data_path:str = field(
-        default='primeqa/qg/sample_table.json', metadata={"help": "Path to JSON file with LIST of tables/passages. Each table \
-                              should be a dict with keys 'header' and 'rows', and passages should be str"}
+    data_path: str = field(
+        default="primeqa/qg/sample_table.json",
+        metadata={
+            "help": "Path to JSON file with LIST of tables/passages. Each table \
+                              should be a dict with keys 'header' and 'rows', and passages should be str"
+        },
     )
     generate_aggregate: Optional[bool] = field(
-        default=False, metadata={"help": "Whether to generate aggregate questions with max, min, sum, etc."}
+        default=False,
+        metadata={
+            "help": "Whether to generate aggregate questions with max, min, sum, etc."
+        },
     )
     gen_output_path: Optional[str] = field(
-        default='sample_generation.json', metadata={"help": "path to JSON file where generated questions will be saved"} 
+        default="sample_generation.json",
+        metadata={"help": "path to JSON file where generated questions will be saved"},
     )
+
 
 def main(raw_args):
     print(raw_args)
-    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, Seq2SeqTrainingArguments, InferenceArguments))
+    parser = HfArgumentParser(
+        (
+            ModelArguments,
+            DataTrainingArguments,
+            Seq2SeqTrainingArguments,
+            InferenceArguments,
+        )
+    )
 
     if len(raw_args) == 2 and raw_args[1].endswith(".json"):
-        model_args, data_args, training_args, inference_args = parser.parse_json_file(json_file=raw_args[1])
+        model_args, data_args, training_args, inference_args = parser.parse_json_file(
+            json_file=raw_args[1]
+        )
     elif len(raw_args) == 1:
         model_args, data_args, training_args = parser.parse_dict(raw_args[0])
     else:
-        model_args, data_args, training_args, inference_args = parser.parse_args_into_dataclasses()
-    
+        (
+            model_args,
+            data_args,
+            training_args,
+            inference_args,
+        ) = parser.parse_args_into_dataclasses()
+
     # These arguments has to be hardcoded in order for Trainer to work
-    training_args.predict_with_generate=True
+    training_args.predict_with_generate = True
     training_args.remove_unused_columns = False
     training_args.prediction_loss_only = False
-    
+
     if (
         os.path.exists(training_args.output_dir)
         and os.listdir(training_args.output_dir)
@@ -142,28 +189,33 @@ def main(raw_args):
         dataset_name=data_args.dataset_name,
         modality=model_args.modality,
         input_max_len=data_args.max_len,
-        target_max_len=data_args.target_max_len
-        )
+        target_max_len=data_args.target_max_len,
+    )
     if training_args.do_train or training_args.do_eval:
-        train_dataset = qgdl.create(data_split="train", data_config=data_args.dataset_config)
-        valid_dataset = qgdl.create(data_split="validation", data_config=data_args.dataset_config)
+        train_dataset = qgdl.create(
+            data_split="train", data_config=data_args.dataset_config
+        )
+        valid_dataset = qgdl.create(
+            data_split="validation", data_config=data_args.dataset_config
+        )
 
         compute_metrics = rouge_metrics(qg_model.tokenizer)
-        
 
         trainer = Seq2SeqTrainer(
             model=qg_model.model,
-            tokenizer = qg_model.tokenizer,
+            tokenizer=qg_model.tokenizer,
             args=training_args,
             train_dataset=train_dataset,
             eval_dataset=valid_dataset,
             data_collator=T2TDataCollator(),
-            compute_metrics=compute_metrics
+            compute_metrics=compute_metrics,
         )
 
     if training_args.do_train:
         trainer.train(
-        model_path=model_args.model_name_or_path if os.path.isdir(model_args.model_name_or_path) else None
+            model_path=model_args.model_name_or_path
+            if os.path.isdir(model_args.model_name_or_path)
+            else None
         )
         train_result = trainer.save_model()
 
@@ -171,35 +223,32 @@ def main(raw_args):
         trainer.log_metrics("train", metrics)
         trainer.save_metrics("train", metrics)
         trainer.save_state()
-    
+
     # Inference
     if inference_args.do_generate:
         # There are some arguments to control the type of questions generated such as probability of aggregations, number of where clauses etc. (contd.)
-        # These arguments can optionally be provided by the user as inference arguments. 
-        # Check out the notebook at primeqa/notebooks/qg/tableqginference.ipynb for more details.    
-        
+        # These arguments can optionally be provided by the user as inference arguments.
+        # Check out the notebook at primeqa/notebooks/qg/tableqginference.ipynb for more details.
+
         # aggregation proobablities
-        agg_prob = [1., 0., 0., 0., 0., 0.]
+        agg_prob = [1.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         if inference_args.generate_aggregate:
-            agg_prob = [0., 0.2, 0.2, 0.2, 0.2, 0.2]
-        
+            agg_prob = [0.0, 0.2, 0.2, 0.2, 0.2, 0.2]
+
         # where clauses
-        where_prob = [0.]*5
+        where_prob = [0.0] * 5
         for i in range(1, 5):
             if i <= inference_args.max_where_clauses:
-                where_prob [i] = 1.
-        where_prob = [w/sum(where_prob) for w in where_prob]
+                where_prob[i] = 1.0
+        where_prob = [w / sum(where_prob) for w in where_prob]
 
         with open(inference_args.data_path) as fp:
             data_list = json.load(fp)
-        
+
         generated_questions = qg_model.generate_questions(
-                                data_list,
-                                inference_args.num_questions_per_instance,
-                                agg_prob,
-                                where_prob
-                                )
-        with open(inference_args.gen_output_path, 'w') as fp:
+            data_list, inference_args.num_questions_per_instance, agg_prob, where_prob
+        )
+        with open(inference_args.gen_output_path, "w") as fp:
             json.dump(generated_questions, fp)
 
     # Evaluation
@@ -213,9 +262,10 @@ def main(raw_args):
             for key in sorted(metrics.keys()):
                 logger.info("  %s = %s", key, str(metrics[key]))
                 writer.write("%s = %s\n" % (key, str(metrics[key])))
-        
+
         trainer.log_metrics("eval", metrics)
         trainer.save_metrics("eval", metrics)
+
 
 if __name__ == "__main__":
     main(sys.argv)
