144ca065d5fa524c9b4acfea3cca6c01ae49a205
Maximilian Schmidt <maximilian.schmidt@ims.uni-stuttgart.de>
2022-09-13 13:11:35 +0200

    Address PR feedback regarding QGTrainer
     - Add placeholder init method for QGTrainer

--- primeqa/qg/trainers/qg_trainer.py
+++ primeqa/qg/trainers/qg_trainer.py
@@ -2,4 +2,7 @@ from transformers import Seq2SeqTrainer
 
 class QGTrainer(Seq2SeqTrainer):
     """ The trainer class for QG. All related functionality should go to this class. """
-    pass
+
+    def __init__(self, *args, **kwargs):
+        """ Custom intialization for the QGTrainer should be added here. """
+        super().__init__(*args, **kwargs)
