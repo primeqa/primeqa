2d8637fb5a64724d943c9be46589cb71c0a9134d
zhrong <zhangr@us.ibm.com>
2022-10-20 01:48:04 -0400

    add scorer to include long answer type score

--- primeqa/mrc/processors/postprocessors/scorers.py
+++ primeqa/mrc/processors/postprocessors/scorers.py
@@ -14,6 +14,7 @@ class SupportedSpanScorers(Enum):
     SCORE_DIFF_BASED = 'score_diff_based'
     TARGET_TYPE_WEIGHTED_SCORE_DIFF = 'target_type_weighted_score_diff'
     WEIGHTED_SUM_TARGET_TYPE_AND_SCORE_DIFF = 'weighted_sum_target_type_and_score_diff'
+    WEIGHTED_SUM_SHORT_LONG_TARGET_TYPE_AND_SCORE_DIFF = 'weighted_sum_short_long_target_type_and_score_diff'
 
     @classmethod
     def get_supported(cls):
@@ -56,6 +57,16 @@ def initialize_scorer(scorer_type: Union[str, SupportedSpanScorers],
         return partial(
             compute_weighted_sum_short_answer_type_score_diff_between_span_and_cls,
             target_type_weight=target_type_weight)
+    elif scorer_type == SupportedSpanScorers.WEIGHTED_SUM_SHORT_LONG_TARGET_TYPE_AND_SCORE_DIFF:
+        logging.debug(
+            "\tInitialized scorer %s with weight %s for the short and long answer target type score and weight %s for "
+            "the score diff score" % (
+                compute_weighted_sum_short_and_long_answer_type_score_diff_between_span_and_cls.__name__,
+                target_type_weight, 1 - target_type_weight))
+
+        return partial(
+            compute_weighted_sum_short_and_long_answer_type_score_diff_between_span_and_cls,
+            target_type_weight=target_type_weight)
     else:
         raise ValueError('Unsupported scorer type: %s' % scorer_type)
 
@@ -91,3 +102,17 @@ def compute_weighted_sum_short_answer_type_score_diff_between_span_and_cls(
 
     return (1 - target_type_weight) * score_diff + target_type_weight * target_type_logits[
         int(TargetType.SPAN_ANSWER)]
+
+def compute_weighted_sum_short_and_long_answer_type_score_diff_between_span_and_cls(
+        span_score: float, null_span_score: float,
+        target_type_logits: List, target_type_weight: float, *args, **kwargs):
+    """
+    Compute score as sum of products `target_type_weight` times target type logit (for span answer) and
+    `1 - target_type_weight` times `compute_score_diff_between_span_and_cls`.
+    """
+    score_diff = compute_score_diff_between_span_and_cls(
+        span_score=span_score, null_span_score=null_span_score)
+
+    return (1 - target_type_weight) * score_diff + target_type_weight * \
+           (target_type_logits[int(TargetType.PASSAGE_ANSWER)] +
+            target_type_logits[int(TargetType.SPAN_ANSWER)])
