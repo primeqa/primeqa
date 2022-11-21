9c4a50cf374e76133b2853eee91db53094eccb58
vishwajeet <vishk024@in.ibm.com>
2022-11-21 04:10:13 -0500

    added path to wikilinks in table_utils and added preprocessing code

--- primeqa/hybridqa/utils/table_utils.py
+++ primeqa/hybridqa/utils/table_utils.py
@@ -1,7 +1,7 @@
 import json
 
 
-resource_path = "/dccstor/cssblr/vishwajeet/git/tabtextqa/data/WikiTables-WithLinks"
+resource_path = "data/hybridqa/WikiTables-WithLinks"
 
 def load_data(table_id):
     with open('{}/request_tok/{}.json'.format(resource_path, table_id)) as f:
