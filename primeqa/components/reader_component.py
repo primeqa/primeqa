b5196f07e46e5a4c6d5296f6c5933a1cbde30655
Jaydeep Sen <jaydesen@in.ibm.com>
2022-12-01 18:12:44 -0500

    tapex model class integration with reader

---
+++ primeqa/components/reader_component.py
@@ -0,0 +1,20 @@
+from primeqa.tableqa.tapex.models.tapex_model import TapexModel
+class ReaderComponent():
+    
+    def __init__(self,runnable_class_name,path_to_config_json=None):
+        # if path_to_config_json is None:
+        #     path_to_config_json = os.path.realpath(__file__)+runnable_class_name+"_config.json"
+        self._class_object = eval(runnable_class_name)(path_to_config_json)
+    
+    def predict(self, data, queries):
+        answers = self._class_object.predict(data,queries)
+        return answers
+    
+    def train(self):
+        trained_results = self._class_object.train()
+        return trained_results
+
+    def evaluate(self):
+        eval_results = self._class_object.eval()
+        return eval_results
+
