bd0f11a57579c9c62814ad362c1ca2bfd54b081a
Jaydeep Sen <jaydesen@in.ibm.com>
2022-12-05 06:06:24 -0500

    importing argument utis in run_tapex

--- primeqa/tableqa/tapex/utils/argument_utils_for_tapex.py
+++ primeqa/tableqa/tapex/utils/argument_utils_for_tapex.py
@@ -162,6 +162,18 @@ class DataTrainingArguments:
             "help": "Whether to ignore the tokens corresponding to padded labels in the loss computation or not."
         },
     )
-            
+    
+    def __post_init__(self):
+        if self.dataset_name is None and self.train_file is None and self.validation_file is None:
+            raise ValueError("Need either a dataset name or a training/validation file.")
+        else:
+            if self.train_file is not None:
+                extension = self.train_file.split(".")[-1]
+                assert extension in ["csv", "json"], "`train_file` should be a csv or a json file."
+            if self.validation_file is not None:
+                extension = self.validation_file.split(".")[-1]
+                assert extension in ["csv", "json"], "`validation_file` should be a csv or a json file."
+        if self.val_max_target_length is None:
+            self.val_max_target_length = self.max_target_length
             
             
