35f1e4c4f626bf29108bcf1da0364cb098631469
vishwajeet <vishk024@in.ibm.com>
2022-12-05 23:38:37 -0500

    test passing with 66% coverage, moved configs to config folder

--- tests/primeqa/tableqa/models/test_tapex_model.py
+++ tests/primeqa/tableqa/models/test_tapex_model.py
@@ -6,13 +6,15 @@ from transformers import (
     TapexTokenizer
 )
 
-@pytest.mark.parametrize("config_path",["../../primeqa/tableqa/tapex/tapex_config.json"])
+@pytest.mark.parametrize("config_path",["tests/resources/tapex/tapex_config.json"])
 def test_tapex_model(config_path):
     tapex_model = TapexModel(config_path)
     tapex_model.load_model_from_config(config_path)
-    reader = ReaderComponent('TapexModel',"../../primeqa/tableqa/tapex/tapex_config.json")
-    assert reader._class_object=="TapexModel"
+    reader = ReaderComponent('TapexModel',config_path)
+    assert type(reader._class_object)==TapexModel
     assert type(tapex_model.model)== BartForConditionalGeneration
     assert type(tapex_model.tokenizer) == TapexTokenizer
+    
+    
     
     
