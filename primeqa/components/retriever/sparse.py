d40bc758389b1b929958d0e04d94b5f66a982be9
vishwajeet <vishk024@in.ibm.com>
2022-12-23 06:13:05 -0500

    renamed readercomponent to reader, retrievercomponent to retriever and IndexerComponent to indexer everywhere

--- primeqa/components/retriever/sparse.py
+++ primeqa/components/retriever/sparse.py
@@ -4,9 +4,11 @@ import json
 
 from primeqa.pipelines.components.base import RetrieverComponent
 from primeqa.ir.sparse.retriever import PyseriniRetriever
+from primeqa.components.base import Retriever
 
 
 @dataclass
+class BM25Retriever(Retriever):
 class BM25Retriever(Retriever):
     """_summary_
 
