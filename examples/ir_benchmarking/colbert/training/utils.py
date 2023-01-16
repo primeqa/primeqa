ce660af2cc6c003060a36077f49cacb65198b71c
Jon Saad-Falcon <41205309+jonsaadfalcon@users.noreply.github.com>
2023-01-16 09:25:49 -0500

    Update utils.py
    Signed-off-by: Jon Saad-Falcon <41205309+jonsaadfalcon@users.noreply.github.com>

--- examples/ir_benchmarking/colbert/training/utils.py
+++ examples/ir_benchmarking/colbert/training/utils.py
@@ -1,7 +1,6 @@
 import os
 import torch
 
-# from colbert.utils.runs import Run
 from colbert.utils.utils import print_message, save_checkpoint
 from colbert.parameters import SAVED_CHECKPOINTS
 from colbert.infra.run import Run
@@ -13,10 +12,7 @@ def print_progress(scores):
 
 
 def manage_checkpoints(args, colbert, optimizer, batch_idx, savepath=None, consumed_all_triples=False):
-    # arguments = dict(args)
-
-    # TODO: Call provenance() on the values that support it??
-
+    
     checkpoints_path = savepath or os.path.join(Run().path_, 'checkpoints')
     name = None
 
@@ -31,13 +27,9 @@ def manage_checkpoints(args, colbert, optimizer, batch_idx, savepath=None, consu
     path_save = None
 
     if consumed_all_triples or (batch_idx % 2000 == 0):
-        # name = os.path.join(path, "colbert.dnn")
-        # save_checkpoint(name, 0, batch_idx, colbert, optimizer, arguments)
         path_save = os.path.join(checkpoints_path, "colbert")
 
     if batch_idx in SAVED_CHECKPOINTS:
-        # name = os.path.join(path, "colbert-{}.dnn".format(batch_idx))
-        # save_checkpoint(name, 0, batch_idx, colbert, optimizer, arguments)
         path_save = os.path.join(checkpoints_path, f"colbert-{batch_idx}")
 
     if path_save:
@@ -45,10 +37,6 @@ def manage_checkpoints(args, colbert, optimizer, batch_idx, savepath=None, consu
 
         checkpoint = {}
         checkpoint['batch'] = batch_idx
-        # checkpoint['epoch'] = 0
-        # checkpoint['model_state_dict'] = model.state_dict()
-        # checkpoint['optimizer_state_dict'] = optimizer.state_dict()
-        # checkpoint['arguments'] = arguments
 
         save(path_save)
 
