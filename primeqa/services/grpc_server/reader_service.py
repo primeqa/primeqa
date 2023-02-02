4f95a4ebb4086732eb5f8b9a187653ba31d8d386
Kshitij Fadnis <cool.kshij@gmail.com>
2023-02-02 02:20:26 +0000

    fix (service): Update reader proto to support optional parameters.

--- primeqa/services/grpc_server/reader_service.py
+++ primeqa/services/grpc_server/reader_service.py
@@ -170,18 +170,18 @@ class ReaderService(ReadingServiceServicer):
                                         answers=[
                                             Answer(
                                                 text=prediction["span_answer_text"],
-                                                start_char_offset=prediction[
-                                                    "span_answer"
-                                                ]["start_position"],
-                                                end_char_offset=prediction[
-                                                    "span_answer"
-                                                ]["end_position"],
                                                 confidence_score=prediction[
                                                     "confidence_score"
                                                 ],
                                                 context_index=int(
                                                     prediction["example_id"]
                                                 ),
+                                                start_char_offset=prediction[
+                                                    "span_answer"
+                                                ]["start_position"],
+                                                end_char_offset=prediction[
+                                                    "span_answer"
+                                                ]["end_position"],
                                             )
                                             for prediction in predictions_for_context
                                         ]
