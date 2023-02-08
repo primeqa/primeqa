2b4ac764b76eff9d27d97c46eca5ec2fb4b47c6e
Sara <sjrosenthal@us.ibm.com>
2023-02-07 22:50:04 -0500

    first commit for baselines

---
+++ examples/llm/analyze_llms.py
@@ -0,0 +1,172 @@
+from primeqa.components.reader.LLMService import LLMService
+from primeqa.components.reader.prompt import BAMReader
+import json
+import sys
+import os
+from dataclasses import dataclass, field, asdict
+from rouge import Rouge
+import numpy as np
+import logging
+from transformers import HfArgumentParser
+
+# BAM docsL https://bam.res.ibm.com/docs/api-reference
+
+# read in ELI5 dev data and run through LLM service.
+
+@dataclass
+class LLMAnalyzeArguments:
+    """
+    Arguments pertaining to processing nq.
+    """
+    api_key: str = field(
+        metadata={"help": "The API key for BAM https://bam.res.ibm.com/"},
+    )
+    model_name: str = field(
+        default="google/flan-t5-xxl",
+        metadata={"help": "Model"},
+    )
+    prefix: str = field(
+        default="Answer the following question after looking at the text. ",
+        metadata={"help": "prefix for the LLM"},
+    )
+    prefix_name: str = field(
+        default="default",
+        metadata={"help": "The abbreviated name to give the prefix (for naming the directory)"},
+    )
+    max_tokens: int = field(
+        default=1024,
+        metadata={
+            "help": "Maximum length of question and context inputs to the model (in word pieces/bpes)",
+        },
+    )
+    min_tokens: int = field(
+     default=100,
+        metadata={
+            "help": "Minimum new tokens that must be generated (in word pieces/bpes)",
+        },   
+    )
+    temperature: float = field(
+        default=0, metadata={"help": "The temperature parameter used for generation"}
+    )
+    top_p: int = field(
+        default=1, metadata={"help": "The top_p parameter used for generation"}
+    )
+    top_k: int = field(
+        default=5, metadata={"help": "The top_p parameter used for generation"}
+    )
+    subset_start: int = field(
+        default=-1,
+        metadata={'help': 'start offset to process a subset of the dataset'}
+    )
+    subset_end: int = field(
+        default=-1,
+        metadata={'help': 'end offset to process a subset of the dataset'}
+    )
+    output_dir: str= field(
+        default='/output/loc/here/jsonl', 
+        metadata={"help": "directory to output file(s) in tydi google format. (jsonl)"}
+    )
+    use_passages: bool = field(
+        default=False, metadata={"help": "If true input passages to the LLM (up to 3)"}
+    )
+    n_shot: int = field(
+        default = 0,
+        metadata={'help': 'number of examples *with* answers to provide to the LLM (0, 1, 2)'}
+    )
+
+def rougel_score(prediction, ground_truth):
+    rouge = Rouge()
+    # no normalization
+    try:
+        scores = rouge.get_scores(prediction, ground_truth, avg=True)
+    except ValueError:  # "Hypothesis is empty."
+        return 0.0
+    return scores["rouge-l"]["f"]
+
+def metric_max_over_ground_truths(prediction, ground_truths):
+    scores_for_ground_truths = []
+    for ground_truth in ground_truths:
+        score = rougel_score(prediction, ground_truth['answer'])
+        scores_for_ground_truths.append(score)
+    return max(scores_for_ground_truths)
+
+def load_jsonl(file_name):
+    logging.info("Load file: " + file_name)
+    json_lines = []
+    with open (file_name, 'r') as f:
+        data_lines = f.readlines()
+        for line in data_lines:
+           json_lines.append(json.loads(line))
+    return json_lines
+
+def get_answer(service, instance, args, n_doc=3):
+    passages = []
+
+    if args.use_passages:
+        i = 0
+        for t in instance["passages"]:
+            i += 1
+            passages.append(t["text"])
+            if i >= n_doc:
+                break
+    r = service.predict([instance['input']], [passages], **asdict(args))
+
+    return metric_max_over_ground_truths(r[0]['text'], instance['output']), r[0]['text']
+
+def get_examples(n_shot=1):
+   return None
+
+def main():
+
+    count = 0
+    avg_rougeL = 0
+    
+    parser = HfArgumentParser(LLMAnalyzeArguments)
+    args = parser.parse_args_into_dataclasses()[0]
+    
+    reader = BAMReader(args)
+    reader.load(model=args.model_name)
+
+    dev_file = "/dccstor/srosent2/primeqa-mengxia/data/dpr-100passages_withkg_best_all/eli5-dev-kilt-dpr-kg-00.json"
+    reference_data = load_jsonl(dev_file)
+
+    model_dir = args.model_name.replace("/","-") + "/prefix_" + args.prefix_name + "-passages_" + str(args.use_passages) + "-" + \
+        str(args.n_shot) + "shot-pktemp" + str(args.top_p) + "_" + str(args.top_k) + "_" + str(args.temperature) \
+        + "-minmaxtok_" + str(args.min_tokens) + "_" + str(args.max_tokens)
+
+    # generate a unique name for this directory so that we can identify based on the dir
+
+    if not os.path.isdir(args.output_dir):
+        logging.error("Missing output directory " + args.output_dir)
+        sys.exit(0)
+    elif not os.path.isdir(args.output_dir + "/" + model_dir):
+        os.makedirs(args.output_dir + "/" + model_dir)
+
+    if args.subset_start == -1:
+        args.subset_start = 0
+    if args.subset_end == -1 or int(args.subset_end) > len(reference_data):
+        args.subset_end = len(reference_data)
+
+    # if os.path.exists(args.output_dir + "/" + model_dir + "/" + 'results-' + str(args.subset_start) + "-" + str(args.subset_end) + '.json'):
+    #     logging.error(args.output_dir + "/" + model_dir + " exists and is not empty")
+    #     sys.exit(0)
+    fp = open(args.output_dir + "/" + model_dir + "/" + 'results-' + str(args.subset_start) + "-" + str(args.subset_end) + '.json', 'w')
+    
+    for instance in reference_data[args.subset_start:args.subset_end]:
+        answer = {}
+        rouge, text = get_answer(reader, instance, args)
+        answer['rouge'] = rouge
+        answer['text'] = text
+        answer['id'] = instance['id']
+        answer['question'] = instance['input']
+        json.dump(answer, fp)
+        fp.write("\n")
+        avg_rougeL += rouge
+        count += 1
+        if count % 10 == 0:
+            logging.info(count, flush=True)
+    fp.close()   
+    logging.info("RougeL: " + str(avg_rougeL/count))
+
+if __name__ == '__main__':
+   main()
