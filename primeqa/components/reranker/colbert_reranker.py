6a8143937da1a8b378b6532487626c35b9f866a4
Martin Franz <102831333+franzmpub@users.noreply.github.com>
2023-03-21 08:25:06 -0400

    Update primeqa/components/reranker/colbert_reranker.py
    Co-authored-by: seirasto <sjrosenthal@us.ibm.com>
    Signed-off-by: Martin Franz <102831333+franzmpub@users.noreply.github.com>

--- primeqa/components/reranker/colbert_reranker.py
+++ primeqa/components/reranker/colbert_reranker.py
@@ -20,7 +20,7 @@ class ColBERTReranker(BaseReranker):
     Important:
     1. Each field has metadata property which can carry additional information for other downstream usages.
     2. Two special keys (api_support and exclude_from_hash) are defined in "metadata" property.
-        a. api_support (bool, optional): If set to True, that parameter is exposed via service layer. Defaults to False.
+        a. api_support (bool, optional): If set to True, that parameter is exposed via the service layer. Defaults to False.
         b. exclude_from_hash (bool,optional): If set to True, that parameter is not considered while building the hash representation for the object. Defaults to False.
 
     Returns:
