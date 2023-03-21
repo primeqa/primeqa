e2a71f5e5cac90f6ce084227254a0a0071ac166d
Bhavani Iyer <69366014+bhavani105@users.noreply.github.com>
2023-03-21 15:26:02 -0400

    Update primeqa/services/exceptions.py
    Co-authored-by: seirasto <sjrosenthal@us.ibm.com>
    Signed-off-by: Bhavani Iyer <69366014+bhavani105@users.noreply.github.com>

--- primeqa/services/exceptions.py
+++ primeqa/services/exceptions.py
@@ -41,4 +41,4 @@ class ErrorMessages(str, Enum):
     INVALID_RETRANKER = "E5001: Invalid reranker: {}. Please select one of the following pre-defined rerankers: {}"
 
     # INITIALIZATION
-    FAILED_TO_INITIALIZE = "E9001: Failed to initalize {}. Please contact us."
+    FAILED_TO_INITIALIZE = "E9001: Failed to initialize {}. Please contact us."
