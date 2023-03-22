90956855155335bc0c31c0f1f939e2ac5e69100b
franzm <franzm@us.ibm.com>
2023-03-22 10:53:26 -0400

    Addressed comments from PR

--- primeqa/ir/dense/colbert_top/colbert/modeling/hf_colbert_roberta.py
+++ primeqa/ir/dense/colbert_top/colbert/modeling/hf_colbert_roberta.py
@@ -6,7 +6,6 @@ from primeqa.ir.dense.colbert_top.colbert.utils.utils import torch_load_dnn
 from primeqa.ir.dense.colbert_top.colbert.utils.utils import print_message
 
 class HF_ColBERT_Roberta(RobertaModel):
-#class HF_ColBERT_XLMR(PreTrainedModel):
     """
         Shallow wrapper around HuggingFace transformers. All new parameters should be defined at this level.
         
@@ -17,27 +16,16 @@ class HF_ColBERT_Roberta(RobertaModel):
         super().__init__(config)
 
         self.dim = colbert_config.dim
-        # resolve conflict between bert and roberta
-        # self.roberta = XLMRobertaModel(config)
-        # self.bert = self.roberta
         self.encoder = None
         self.embeddings = None
         self.pooler = None
 
         self.roberta = RobertaModel(config)
         self.bert = self.roberta
-        #self.bert = XLMRobertaModel(config)
-
         self.linear = nn.Linear(config.hidden_size, colbert_config.dim, bias=False)
-
-        # if colbert_config.relu:
-        #     self.score_scaler = nn.Linear(1, 1)
 
         self.init_weights()
 
-        # if colbert_config.relu:
-        #     self.score_scaler.weight.data.fill_(1.0)
-        #     self.score_scaler.bias.data.fill_(-8.0)
 
     @classmethod
     def from_pretrained(cls, name_or_path, colbert_config):
@@ -53,14 +41,10 @@ class HF_ColBERT_Roberta(RobertaModel):
             from collections import OrderedDict
             import re
 
-            # for reading V1
-            # state_dict = OrderedDict([(re.sub(r'^roberta.', 'bert.', key), value) for key, value in state_dict.items()])
-
             # for reading V2
             state_dict = OrderedDict([(re.sub(r'^model.', '', key), value) for key, value in state_dict.items()])
 
             obj = super().from_pretrained(base, state_dict=state_dict, colbert_config=colbert_config)
-            #obj = super().from_pretrained(base, state_dict=dnn['model_state_dict'], colbert_config=colbert_config)
             obj.base = base
 
             return obj
@@ -86,11 +70,3 @@ class HF_ColBERT_Roberta(RobertaModel):
         obj.base = name_or_path
 
         return obj
-
-"""
-TODO: It's easy to write a class generator that takes "name_or_path" and loads AutoConfig to check the Architecture's
-      name, finds that name's *PreTrainedModel and *Model in dir(transformers), and then basically repeats the above.
-
-      It's easy for the BaseColBERT class to instantiate things from there.
-"""
-
--- primeqa/ir/dense/colbert_top/colbert/modeling/tokenization/doc_tokenization_roberta.py
+++ primeqa/ir/dense/colbert_top/colbert/modeling/tokenization/doc_tokenization_roberta.py
@@ -26,10 +26,6 @@ class DocTokenizerRoberta():
     def tensorize(self, batch_text, bsize=None):
         assert type(batch_text) in [list, tuple], (type(batch_text))
 
-        # add placehold for the [D] marker
-        # strangely, prefixing with '. ' introduces _two_ extra tokens [5,6]
-        # it seems that 6 is the empty string
-        # into the output - I don't understand why ...
         batch_text = ['$ ' + x for x in batch_text]
 
         obj = self.tok(batch_text, padding='longest', truncation='longest_first',
@@ -42,14 +38,10 @@ class DocTokenizerRoberta():
 
         if self.used is False:
             self.used = True
-            # firstbg = (context is None) or context[0]
-            # print()
             print_message("#> Roberta DocTokenizer.tensorize(batch_text[0], batch_background[0], bsize) ==")
-            # print(f"#> Input: {batch_text[0]}, \t\t {firstbg}, \t\t {bsize}")
             print_message(f"#> Input: {batch_text[0]}, \t\t {bsize}")
             print_message(f"#> Output IDs: {ids[0].size()}, {ids[0]}")
             print_message(f"#> Output Mask: {mask[0].size()}, {mask[0]}")
-            # print()
 
         if bsize:
             ids, mask, reverse_indices = _sort_by_length(ids, mask, bsize)
@@ -57,23 +49,3 @@ class DocTokenizerRoberta():
             return batches, reverse_indices
 
         return ids, mask
-
-
-# In [1]: from colbert.modeling.tokenization import DocTokenizerXLMR
-
-# In [2]: t=DocTokenizerXLMR(50)
-
-# In [3]: t.tensorize(['Here is the answer.', 'Another longer answer is here.'])
-# (tensor([[     0,   9749,  11853,     83,     70,  35166,      5,      2,      1],
-#          [     0,   9749, 116267,  51713,  35166,     83,   3688,      5,      2]]),
-#  tensor([[1, 1, 1, 1, 1, 1, 1, 1, 0],
-#          [1, 1, 1, 1, 1, 1, 1, 1, 1]]))
-
-
-# In [4]:  t.D_marker_token_id
-# Out[4]: 9749
-
-
-# In [5]: t.tok.decode(range(6))
-# Out[6]: '<s><pad></s><unk>,.'
-
