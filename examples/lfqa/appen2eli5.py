e0441c9834c5846f3113603ab6bccfd7c9aab5fe
Sara <sjrosenthal@us.ibm.com>
2023-03-28 13:19:53 -0400

    updates for round 2 and output to eli5

--- examples/lfqa/appen2eli5.py
+++ examples/lfqa/appen2eli5.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from datetime import datetime
 import json
+import glob
 
 name = {47200615:"Hee Dong", 46545976: "Eva Maria", 46092070: "Sara", 45676624: "Mohamed", 46373812: "Chie", 46545946: "Joekie", 46954475: "Arafat", 46994197: "Salim"}
 
@@ -30,7 +31,7 @@ ck will usually run when a passing play has broken down – such as when there i
 
 def process_data(data, valid_annotators=None, answer_field=""):
 
-    eli5_formatted_data = []
+    eli5_formatted_data = {}
     answered = 0
     non_consecutive = 0
     selected = 0
@@ -76,7 +77,7 @@ def process_data(data, valid_annotators=None, answer_field=""):
             answer_type = judgement['data']['how_would_you_describe_the_questionanswer']
 
             output['answer'] = ""
-            output['meta'] = {"score":0, "annotator": name[judgement['worker_id']], "has_minimal_answer": has_minimal_answer}
+            output['meta'] = {"score":0, "annotator": [name[judgement['worker_id']]], "has_minimal_answer": has_minimal_answer}
             
             if answer_type == 'complete' or answer_type == 'partial':
                 # error
@@ -87,24 +88,92 @@ def process_data(data, valid_annotators=None, answer_field=""):
                 is_answered = True
                 output['answer'] = judgement['data'][answer_field]
                 eli5_format['output'].append(output)
-        if is_answered:
-            answered += 1
-            eli5_formatted_data.append(eli5_format)
+                answered += 1
+            elif answer_type == "skip" or answer_type == "bad_paragraph":
+                output['meta']['score'] = 3
+                output['answer'] = "NA"
+                eli5_format['output'].append(output)
+        if len(eli5_format['output']) > 0: 
+            eli5_formatted_data[eli5_format['id']] = eli5_format
     print("answered: " + str(answered) + "/" + str(len(eli5_formatted_data)))
     print("non-consective: " + str(non_consecutive))
     print("selected: " + str(selected / len(eli5_formatted_data)))
     print("num sentences: " + str(num_sentences / len(eli5_formatted_data)))
     return eli5_formatted_data, answered, non_consecutive
 
-def main():
-    file_names = ["/dccstor/srosent2/generative/appen/job_2022794.json", "/dccstor/srosent2/generative/appen/job_2035917.json", 
-    "/dccstor/srosent2/generative/appen/job_2006984.json", "/dccstor/srosent2/generative/appen/job_2004889.json"]
-    answer_fields = ["type_your_answer_here_it_should_be_concise_and_only_come_from_the_passagetitle_", "type_your_answer_here_it_should_be_concise_and_only_come_from_the_passagetitle_",
-        "type_your_answer_here_it_should_be_concise_and_only_come_from_the_passagetitle_","type_your_answer_here_keep_your_answer_as_close_to_the_passage_as_possible_"]
-    valid_annotators = [None, None, ["Arafat","Sara","Salim"], ["Arafat","Sara","Salim"]]
-    output_file = "/dccstor/srosent2/generative/appen/NQ_formatted_answered.json"
+def process_round2_data(eli5_formatted_data, data, answer_field=""):
+    answered = 0
+    num_sentences = 0
 
-    fid_data = []
+    for _, row in data.iterrows():
+        eli5_format = {}
+
+        if row['data']['id'] in eli5_formatted_data:
+            eli5_format = eli5_formatted_data[row['data']['id']]
+        else:
+            eli5_format['id'] = row['data']['id']
+            eli5_format['input'] = row['data']['question']
+
+            passage = {}
+            passage['title'] = row['data']['title']
+            paragraph = ""   
+
+            num_sentences += len(row['data']['long_answer'])
+            for sentence in row['data']['long_answer']:
+                paragraph += sentence + " "
+            passage["text"] = paragraph
+            eli5_format['passages'] = [passage]
+            eli5_format['output'] = []
+
+        is_answered = False
+        for judgement in row['results']['judgments']:
+            output = {}
+
+            has_minimal_answer = False
+
+            if ('did_you_need_to_edit_the_answer' in judgement['data'] and 'skip' not in judgement['data']['did_you_need_to_edit_the_answer']) \
+            or ('did_you_need_to_edit_the_answer_check_all_that_are_applicable' in judgement['data'] and 'skip' not in judgement['data']['did_you_need_to_edit_the_answer_check_all_that_are_applicable']) :
+                is_answered = True
+            else:
+                updated_answer = "NA"
+
+            if 'minimal_text' in judgement['unit_data'] and judgement['unit_data']['minimal_text'] != '':
+                has_minimal_answer = True
+            
+            if 'update_the_answer_here_if_needed' in judgement['data']:
+                updated_answer = judgement['data']['update_the_answer_here_if_needed']
+                output['answer'] = updated_answer
+                output['meta'] = {"score":0, "annotator": [name[judgement['worker_id']]], "has_minimal_answer": has_minimal_answer}
+                output['meta']['score'] = 3
+                eli5_format['output'].append(output)
+            elif is_answered:
+                # add annotator to other answer
+                if len(eli5_formatted_data[eli5_format['id']]['output']) > 1:
+                    print("Multiple answers already")
+                    print(eli5_format)
+                eli5_formatted_data[eli5_format['id']]['output'][0]['meta']['annotator'].append(name[judgement['worker_id']])
+        if is_answered:
+            answered += 1
+        eli5_formatted_data[eli5_format['id']] = eli5_format
+    print("answered: " + str(answered) + "/" + str(len(eli5_formatted_data)))
+    print("num sentences: " + str(num_sentences / len(eli5_formatted_data)))
+    return eli5_formatted_data, answered
+
+def main():
+    # these are the round 1 files
+    file_names = ["/dccstor/srosent2/generative/appen/round1_jobs/job_2022794.json", "/dccstor/srosent2/generative/appen/round1_jobs/job_2035917.json", 
+    "/dccstor/srosent2/generative/appen/round1_jobs/job_2006984.json", "/dccstor/srosent2/generative/appen/round1_jobs/job_2004889.json", "/dccstor/srosent2/generative/appen/round1_jobs/job_2084633.json"]
+    # these are the round 2 files. 
+    round2_files = glob.glob("/dccstor/srosent2/generative/appen/round2_jobs/*")
+
+    # If an example has multiple annotations keep it as dev set.    
+
+    answer_fields = ["type_your_answer_here_it_should_be_concise_and_only_come_from_the_passagetitle_", "type_your_answer_here_it_should_be_concise_and_only_come_from_the_passagetitle_",
+        "type_your_answer_here_it_should_be_concise_and_only_come_from_the_passagetitle_","type_your_answer_here_keep_your_answer_as_close_to_the_passage_as_possible_", "type_your_answer_here_it_should_be_concise_and_only_come_from_the_passagetitle_"]
+    valid_annotators = [None, None, ["Arafat","Sara","Salim"], ["Arafat","Sara","Salim"], None]
+    output_file = "/dccstor/srosent2/generative/appen/NQ_formatted_answered_single-3.28.23.json"
+
+    fid_data = {}
 
     answered = 0
     non_consecutive = 0
@@ -112,15 +181,28 @@ def main():
     for input_file in file_names:
         data = pd.read_json(input_file, lines=True)
         formatted_data, answered_i, non_consecutive_i = process_data(data, valid_annotators=valid_annotators[i], answer_field=answer_fields[i])
-        fid_data.extend(formatted_data)
+        fid_data.update(formatted_data)
         answered += answered_i
         non_consecutive += non_consecutive_i
         i += 1
     print("answered: " + str(answered) + "/" + str(len(fid_data)))
     print("non-consective: " + str(non_consecutive))
+
+    for input_file in round2_files:
+        round2data = pd.read_json(input_file, lines=True)
+        fid_data, answered_i = process_round2_data(fid_data, round2data)
+
+    two_annotator_data = []
     with open(output_file,'wb') as writer:
         for data in fid_data:
-            writer.write((json.dumps(data) + "\n").encode())
+            if len(fid_data[data]['output']) > 1 or len(fid_data[data]['output'][0]["meta"]["annotator"]) > 1:
+                two_annotator_data.append(fid_data[data])
+                continue
+            writer.write((json.dumps(fid_data[data]) + "\n").encode())
+
+    with open("/dccstor/srosent2/generative/appen/NQ_formatted_answered_multiple-3.28.23.json",'wb') as writer:
+        for data in two_annotator_data:
+            writer.write((json.dumps(two_annotator_data) + "\n").encode())
 
     
 
