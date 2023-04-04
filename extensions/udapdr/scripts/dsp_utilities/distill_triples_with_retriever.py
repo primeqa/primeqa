d6f99e408dca03cd9b86088aedf84870d9d60562
Jon Saad-Falcon <41205309+jonsaadfalcon@users.noreply.github.com>
2023-04-04 12:44:53 -0400

    Update distill_triples_with_retriever.py
    Signed-off-by: Jon Saad-Falcon <41205309+jonsaadfalcon@users.noreply.github.com>

--- extensions/udapdr/scripts/dsp_utilities/distill_triples_with_retriever.py
+++ extensions/udapdr/scripts/dsp_utilities/distill_triples_with_retriever.py
@@ -8,9 +8,9 @@ def distill_triples_with_retriever(given_triples, given_synth_queries, given_che
     with Run().context(RunConfig(nranks=4)):
         
         if LoTTE_or_BEIR == "LoTTE":
-        	collection = '../ColBERT_FM/downloads/lotte/' + chosen_split + '/' + chosen_set +'/collection.tsv'
+        	collection = '../downloads/lotte/' + chosen_split + '/' + chosen_set +'/collection.tsv'
         elif LoTTE_or_BEIR == "BEIR":
-        	collection = '../ColBERT_FM/beir_datasets/' + chosen_BEIR_set + '/' + chosen_BEIR_type +'/collection.tsv'
+        	collection = '../beir_datasets/' + chosen_BEIR_set + '/' + chosen_BEIR_type +'/collection.tsv'
 
         config = ColBERTConfig(bsize=16, lr=1e-05, warmup=None, doc_maxlen=300, dim=128, nway=16, accumsteps=2, use_ib_negatives=False)
         trainer = Trainer(triples=given_triples, queries=given_synth_queries, collection=collection, config=config)
