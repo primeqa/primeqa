664b09722aa6543550ec493ca41bf988af8b9467
Jon Saad-Falcon <41205309+jonsaadfalcon@users.noreply.github.com>
2023-04-12 06:51:44 -0400

    Update distill_triples_with_retriever.py
    Signed-off-by: Jon Saad-Falcon <41205309+jonsaadfalcon@users.noreply.github.com>

--- extensions/udapdr/scripts/dsp_utilities/distill_triples_with_retriever.py
+++ extensions/udapdr/scripts/dsp_utilities/distill_triples_with_retriever.py
@@ -3,16 +3,18 @@ from primeqa.ir.dense.colbert_top.colbert.infra.run import Run
 from primeqa.ir.dense.colbert_top.colbert.infra.config import ColBERTConfig, RunConfig
 from primeqa.ir.dense.colbert_top.colbert import Trainer
 
-def distill_triples_with_retriever(given_triples, given_synth_queries, given_checkpoint, chosen_split, chosen_type, chosen_set, LoTTE_or_BEIR, chosen_BEIR_set, chosen_BEIR_type):
+def distill_triples_with_retriever(given_triples, given_synth_queries, given_checkpoint, chosen_split, chosen_type, chosen_set, dataset, chosen_BEIR_set, chosen_BEIR_type):
     
     nranks = torch.cuda.device_count()
     
     with Run().context(RunConfig(nranks=nranks)):
         
-        if LoTTE_or_BEIR == "LoTTE":
+        if dataset == "LoTTE":
         	collection = '../downloads/lotte/' + chosen_split + '/' + chosen_set +'/collection.tsv'
-        elif LoTTE_or_BEIR == "BEIR":
+        elif dataset == "BEIR":
         	collection = '../beir_datasets/' + chosen_BEIR_set + '/' + chosen_BEIR_type +'/collection.tsv'
+        else:
+            raise ValueError("Incorrect dataset specification for triples")
 
         config = ColBERTConfig(bsize=16, lr=1e-05, warmup=None, doc_maxlen=300, dim=128, nway=16, accumsteps=2, use_ib_negatives=False)
         trainer = Trainer(triples=given_triples, queries=given_synth_queries, collection=collection, config=config)
