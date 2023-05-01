8af98d845d47f0ac6a098f406d983f614bd1b057
Bhavani Iyer <bsiyer@us.ibm.com>
2023-05-01 15:55:54 -0400

    use csv dict reader to load corpus

--- primeqa/ir/dense/colbert_top/utility/utils/qa_loaders.py
+++ primeqa/ir/dense/colbert_top/utility/utils/qa_loaders.py
@@ -1,5 +1,7 @@
 import os
 import ujson
+import csv
+from tqdm import tqdm
 
 from collections import defaultdict
 from primeqa.ir.dense.colbert_top.colbert.utils.utils import print_message, file_tqdm
@@ -7,10 +9,17 @@ from primeqa.ir.dense.colbert_top.colbert.utils.utils import print_message, file
 
 def load_collection_(path, retain_titles):
     with open(path) as f:
+        lines = f.readlines()
+        
+    with open(path) as f:
         collection = []
-
-        for line in file_tqdm(f):
-            _, passage, title = line.strip().split('\t')
+        csv_reader = csv.DictReader(f, fieldnames=["id", "text", "title"], delimiter="\t")
+        for row in tqdm(csv_reader, total=len(lines)):
+            passage = row["text"]
+            title = row["title"]
+            
+        # for line in file_tqdm(f):
+        #     _, passage, title = line.strip().split('\t')
 
             if retain_titles:
                 passage = title + ' | ' + passage
