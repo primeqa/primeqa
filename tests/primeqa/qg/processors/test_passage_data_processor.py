c584d8c0ac8fb5d32fd80dc2286803d70cc606d8
Bhavani Iyer <bsiyer@us.ibm.com>
2023-06-12 15:28:49 -0400

    tydiqa load dataset using streaming

--- tests/primeqa/qg/processors/test_passage_data_processor.py
+++ tests/primeqa/qg/processors/test_passage_data_processor.py
@@ -1,5 +1,5 @@
 import pytest
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
 from primeqa.qg.models.qg_model import QGModel
 from primeqa.qg.processors.passage_qg.qg_processor import QGProcessor
 
@@ -18,7 +18,16 @@ def test_preprocess_data_for_qg(model_name):
     assert len(processed_dataset['question']) == len(processed_dataset['input'])
 
     processor = QGProcessor(tokenizer, input_max_len, target_max_len)
-    dataset = load_dataset('tydiqa', name='secondary_task', split='validation')
+    raw_dataset = load_dataset('tydiqa', name='secondary_task', split='validation', streaming=True)
+    iterable_dataset = raw_dataset.take(100)
+    examples = {}
+    for e in iterable_dataset:
+        for key in e.keys():
+            if key not in examples:
+                examples[key] = []
+            examples[key].append(e[key])
+    dataset =  Dataset.from_dict(examples)
+    
     processed_dataset = processor(dataset)
     assert processed_dataset != None
     assert len(processed_dataset['question']) == len(processed_dataset['input'])
