19ffb1ec2ff432bf660d002d5172ff6774e41001
Sara <sjrosenthal@us.ibm.com>
2023-06-14 14:42:25 -0400

    code to run reader

--- extensions/reader_reranker/reader_reranker.py
+++ extensions/reader_reranker/reader_reranker.py
@@ -2,7 +2,8 @@ import pandas as pd
 import json
 import sys
 
-default_rank_dir = sys.argv[1] #"/dccstor/colbert-ir/bsiyer/vectordb/weaviate/benchmark_1k_q_PassageAllMiniLML6v2/"
+default_rank_dir = sys.argv[1]
+# "/dccstor/colbert-ir/bsiyer/vectordb/weaviate/benchmark_1k_q_PassageAllMiniLML6v2/" 
 
 info_df = pd.read_json(default_rank_dir + "/ranked_passages.tsv.annotated.metrics")
 
@@ -13,7 +14,7 @@ collection_df = pd.read_csv(info_df['arguments']['collection'], delimiter="\t")
 # load model from model hub
 from primeqa.components.reader.extractive import ExtractiveReader
 
-reader = ExtractiveReader(model="PrimeQA/tydiqa-primary-task-xlm-roberta-large", max_num_answers=10)
+reader = ExtractiveReader(model="PrimeQA/nq_tydi_sq1-reader-xlmr_large-20221110", max_num_answers=100)
 reader.load()
 
 
@@ -23,13 +24,15 @@ def run(questions, contexts, example_ids, passage_ids):
         questions, contexts, example_ids=example_ids
     )
     result = {}
+    index = 0
     for i, answers_i in reader_answers.items():
         i_result = {}
         for answer in answers_i:
-            answer['passage_index'] = passage_ids[int(i[i.index('.')+1:])]
+            answer['passage_index'] = passage_ids[index][answer['passage_index']] #[i.index('.')+1:])]
         i_result["answers"] = answers_i
-        i_result["passages"] = contexts[int(i[i.index('.')+1:])]
+        i_result["passages"] = contexts[index] #[i.index('.')+1:])]
         result[i] = i_result
+        index+= 1
     return result
 
 def get_passages(qas, collection, ranking, num_passages=100):
@@ -41,15 +44,18 @@ def get_passages(qas, collection, ranking, num_passages=100):
         q_passages = ranking[ranking['qid'] == row['qid']][:num_passages]
         
         passages = []
+        q_pids = []
         count = 0
         for j, passage in q_passages.iterrows():
-            questions.append(row['question'])
-            contexts.append([collection.iloc[int(passage['pid'])-1]['title'] + "\n" + collection.iloc[int(passage['pid'])-1]['text']])
-            pids.append(str(int(passage['pid'])))
-            example_ids.append(f'{row["qid"]}.{count}')
+            # questions.append(row['question'])
+            passages.append(collection.iloc[int(passage['pid'])-1]['title'] + "\n" + collection.iloc[int(passage['pid'])-1]['text'])
+            q_pids.append(str(int(passage['pid'])))
+            # example_ids.append(f'{row["qid"]}.{count}')
             count+= 1
-        # questions.append(row['question'])
-        # contexts.append(passages)
+        questions.append(row['question'])
+        contexts.append(passages)
+        example_ids.append(f'{row["qid"]}')
+        pids.append(q_pids)
     return questions, contexts, example_ids, pids
 
 start = sys.argv[2]
@@ -60,6 +66,7 @@ q, c, e, p = get_passages(qas_df[int(start):int(end)], collection_df, ranking_df
 
 result = run(q,c,e,p)
 
-with open(f'/dccstor/srosent3/reranking/{sys.argv[4]}/reader_answers_{start}-{end}.json', "w") as outfile:
+output_dir = sys.argv[4]
+with open(f'/dccstor/srosent3/reranking/{output_dir}/reader_answers_{start}-{end}.json', "w") as outfile:
     json.dump(result, outfile)
 
