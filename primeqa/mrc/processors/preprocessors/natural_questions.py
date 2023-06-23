4e67c1e6a29294fa65bf718cce4ec8dfc3b7915d
Sara <sjrosenthal@us.ibm.com>
2023-06-23 14:59:24 -0400

    data stats for NQ and keep_html default false

--- primeqa/mrc/processors/preprocessors/natural_questions.py
+++ primeqa/mrc/processors/preprocessors/natural_questions.py
@@ -62,7 +62,7 @@ class NaturalQuestionsPreProcessor(BasePreProcessor):
             self._single_context_multiple_passages = True
 
 
-    def adapt_dataset(self, dataset: Dataset, is_train: bool, keep_html: bool=True) -> Dataset:
+    def adapt_dataset(self, dataset: Dataset, is_train: bool, keep_html: bool=False) -> Dataset:
         """
         Process dataset examples to rename fields, create context and set answer offset.
         Args:
@@ -83,7 +83,7 @@ class NaturalQuestionsPreProcessor(BasePreProcessor):
         return dataset
 
 
-    def _rename_examples_create_context_and_adjust_offset(self, example: Example, is_train: bool, keep_html: bool=True):
+    def _rename_examples_create_context_and_adjust_offset(self, example: Example, is_train: bool, keep_html: bool=False):
         """
         Rename examples to BasePreProcessor schema,
         create context from document token,
