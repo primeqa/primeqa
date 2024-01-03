0ec93f07aacbe96935a9ae4c7778d10256de7f9b
Radu Florian <raduf@us.ibm.com>
2024-01-03 14:40:48 -0500

    Add docstrings to `extract_langs.py` functions
    Docstrings were added to all of the functions in the `extract_langs.py` script, providing detailed information about the purpose, parameters, and returns of each function. This enhancement improves the readability of the code and assists in understanding the functionality of each component of the script.

--- primeqa/ir/scripts/elastic/extract_langs.py
+++ primeqa/ir/scripts/elastic/extract_langs.py
@@ -7,6 +7,11 @@ LL = len("?locale=")
 
 
 def parse_arguments():
+    """
+    Parses command line arguments.
+    @return: The parsed command line arguments.
+    @rtype: argparse.Namespace
+    """
     parser = ArgumentParser()
     parser.add_argument('-o', '--output', required=True,
                         help='The template output. Languages will be added before the .jsonl extension.')
@@ -16,6 +21,12 @@ def parse_arguments():
 
 
 def get_lang_map(tsv_file):
+    """
+    Generates a language mapping from URLs to languages, based on the URLs - assumes the SAP URL format that contains
+    the language in the URL.
+    @param tsv_file: The path to the TSV (Tab-Separated Values) file containing language mappings.
+    @return: A generator that yields tuples of id and language. Each tuple represents a language mapping found in the TSV file.
+    """
     with open(tsv_file, 'r', encoding='utf8') as inp:
         for i, line in enumerate(inp):
             id, url, title, _ = line.strip().split("\t")
@@ -28,6 +39,18 @@ def get_lang_map(tsv_file):
 
 
 def process_multifile(file_map, multifile, output):
+    """
+    @param file_map: A dictionary that maps language identifiers to file objects. Each file object is used to write the processed data for a specific language.
+    @param multifile: The path to the input file containing the data to be processed.
+    @param output: The path to the output file where the processed data will be written.
+
+    @return: None
+
+    This method processes a multifile containing data and writes separate output files for each language found in the data. The file_map parameter is used to map each language identifier
+    * to a corresponding file object. After processing each line in the multifile, the data is checked for a language identifier. If the language identifier is found in the file_map, the
+    * data is written to the corresponding output file for that language. If the language identifier is not found in the file_map, a message is printed indicating that the language for the
+    * document could not be found. Finally, the file objects in the file_map are closed.
+    """
     for l in file_map.keys():
         file_map[l] = open(output.replace(".jsonl", f"-{l}.jsonl"), 'w')
     with open(multifile, 'r', encoding="utf8") as f:
@@ -44,6 +67,12 @@ def process_multifile(file_map, multifile, output):
 
 
 def close_files(file_map):
+    """
+    @param file_map: A dictionary containing file objects as values.
+    @return: None
+
+    This method iterates through the keys of the file_map dictionary and closes each file object associated with each key.
+    """
     for l in file_map.keys():
         file_map[l].close()
 
